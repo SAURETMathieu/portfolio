6bae322d3f4b0f970521d136a5adadc9f3a330bd
SAURETMathieu <mathieu.sauret@oclock.school>
2024-04-19 15:07:19 +0200

    feat(icons): create components for icons

---
+++ components/icons/BurgerIcon.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { ComponentPropsWithoutRef } from "react";
+
+export const BurgerIcon = (
+  props: ComponentPropsWithoutRef<"svg"> & { size?: number }
+) => {
+  return (
+    <svg
+      width={props.size}
+      height={props.size}
+      viewBox="0 0 448 512"
+      version="1.1"
+      preserveAspectRatio="xMidYMid"
+      {...props}
+    >
+      <path
+        fill="currentColor"
+        d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"
+      />
+    </svg>
+  );
+};
