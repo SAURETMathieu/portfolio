a9b0886a2550762fda8e4923b35ffb3527a52b22
SAURETMathieu <mathieu.sauret@oclock.school>
2024-04-19 15:08:14 +0200

    feat(Navbar): create Navbar component

---
+++ components/header/Navbar.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { GithubIcon } from "../icons/GithubIcon";
+import { LinkedinIcon } from "../icons/LinkedinIcon";
+import { EnvelopeIcon } from "../icons/EnvelopeIcon";
+import { BurgerIcon } from "../icons/BurgerIcon";
+import BurgerMenu from "./BurgerMenu";
+import Link from "next/link";
+
+function Navbar() {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  return (
+    <nav className="flex">
+      <ul className="flex gap-2 py-2">
+        <li>
+          <EnvelopeIcon size={24} className="text-foreground" />
+        </li>
+        <li>
+          <Link href="https://github.com/SAURETMathieu">
+            <GithubIcon size={24} className="text-foreground" />
+          </Link>
+        </li>
+        <li>
+          <Link href="https://www.linkedin.com/in/mathieu-sauret">
+            <LinkedinIcon size={24} className="text-foreground" />
+          </Link>
+        </li>
+        <li className="pl-2">
+          <BurgerMenu isOpen={isOpen} setIsOpen={setIsOpen} />
+        </li>
+      </ul>
+      {isOpen && (
+        <ul className="fixed left-0 top-[61px] opacity-80 bg-primary flex flex-col justify-center items-center gap-2 text-2xl h-screen w-screen z-99">
+          <li>Projets</li>
+          <li>Compétences</li>
+          <li>Expériences</li>
+          <li>Contact</li>
+          <li>...</li>
+        </ul>
+      )}
+    </nav>
+  );
+}
+
+export default Navbar;
