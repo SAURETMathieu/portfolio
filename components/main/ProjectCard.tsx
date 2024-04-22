9cabf0d8f5a07335974100834d76adc5197b97b8
SAURETMathieu <mathieu.sauret@oclock.school>
2024-04-22 23:17:56 +0200

    feat(Project): add ProjectSection and ProjectCard components

---
+++ components/main/ProjectCard.tsx
@@ -0,0 +1,73 @@
+import Image from "next/image";
+import React from "react";
+import DynamicIcon from "./DynamicIcon";
+
+interface Tag {
+  id: number;
+  name: string;
+}
+
+interface LinkType {
+  id: number;
+  name: string;
+  icon: string;
+  url: string;
+}
+
+interface ProjectCardProps {
+  title: string;
+  description: string;
+  imageSrc: string;
+  tags: Tag[];
+  links: LinkType[];
+}
+
+function ProjectCard({
+  title,
+  description,
+  imageSrc,
+  tags,
+  links,
+}: ProjectCardProps) {
+  return (
+    <div className="p-2 flex flex-col justify-between min-w-[288px] sm:min-h-[505px] lg:min-h-[520px] max-w-96 bg-accent/60 border border-current rounded">
+      <div className="flex-none">
+        <Image
+          src={imageSrc}
+          alt={title}
+          width={256}
+          height={144}
+          className="aspect-video w-full h-1/3 object-cover"
+        />
+      </div>
+
+      <div className="p-2 grow">
+        <h3 className="text-xl font-bold flex-start">{title}</h3>
+        <div className="flex justify-center">
+          {links.map((link) => (
+            <a href={link.url} key={link.id}>
+              <span className="hover:scale-105 hover:opacity-90 font-bold bg-accent border border-current text-xs p-1 rounded inline-flex items-center gap-1 m-1 mt-2">
+                <DynamicIcon name={link.icon} size={16} />
+                {link.name}
+              </span>
+            </a>
+          ))}
+        </div>
+        <p className="text-left font-extralight mt-2">{description}</p>
+      </div>
+      <div className="flex flex-none flex-wrap justify-center">
+        {tags.map((tag) => (
+          <span
+            className="font-bold bg-accent border border-current text-xs p-1 rounded inline-flex items-center gap-1 m-1 mt-2"
+            key={tag.id}
+          >
+            <DynamicIcon name={tag.name} size={16} />
+            {tag.name}
+          </span>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default ProjectCard;
---
+++ components/main/ProjectSection.tsx
@@ -0,0 +1,94 @@
+import React from "react";
+import ProjectCard from "./ProjectCard";
+
+function ProjectSection() {
+
+  const projectsCards = [
+    {
+      title: "O'DD",
+      description: `Application de gestion d'élevage de dragodinde (monture) pour le jeu Dofus Rétro.
+      Réalisé en solo dans le but de m'entraîner sur l'ensemble des notions acquises durant ma formation.`,
+      imageSrc: "/images/odd.png",
+      tags: [
+        { id: 1, name: "Vanilla JS" },
+        { id: 2, name: "HTML" },
+        { id: 3, name: "Vanilla CSS" },
+        { id: 4, name: "EJS" },
+        { id: 5, name: "Node.js" },
+        { id: 6, name: "PostgreSQL" },
+      ],
+      links: [
+        { id: 1, name: "Website", url: "https://odd-dofus.up.railway.app/", icon: "Website" },
+        { id: 2, name: "Front/Back", url:"https://github.com/SAURETMathieu/dofus-elevage", icon:"Github" },
+      ],
+    },
+    {
+      title: "KING HIFI",
+      description: `Application pour un véritable groupe de musique (crew) de Sound system Dub.
+      Projet de fin de formation réalisé en groupe de 4, méthode agile, Scrum.`,
+      imageSrc: "/images/odd.png",
+      tags: [
+        { id: 7, name: "React" },
+        { id: 8, name: "Node.js" },
+        { id: 9, name: "Bulma" },
+        { id: 10, name: "Vite" },
+        { id: 11, name: "PostgreSQL" },
+      ],
+      links: [
+        { id: 3, name: "Website", url: "https://pureniceness-front-6a800c130dfe.herokuapp.com/", icon: "Website" },
+        { id: 4, name: "Front", url:"https://github.com/SAURETMathieu/kinghifi-front", icon:"Github" },
+        { id: 5, name: "Back", url:"https://github.com/SAURETMathieu/kinghifi-back", icon:"Github" },
+      ],
+    },
+    {
+      title: "Documentation",
+      description: "Documentation personnelle qui rassemble mes réalisations et mes connaissances, fournissant une référence utile pour mes futurs projets et renforcer mon développement professionnel.",
+      imageSrc: "/images/odd.png",
+      tags: [
+        { id: 12, name: "React" },
+        { id: 13, name: "Docusaurus" },
+        { id: 14, name: "Vite" },
+        { id: 15, name: "Markdown" },
+      ],
+      links: [
+        { id: 6, name: "Website", url: "https://sauretmathieu.github.io/documentation/", icon: "Website" },
+        { id: 7, name: "Front/Back", url:"https://github.com/SAURETMathieu/documentation", icon:"Github" },
+      ],
+    },
+    {
+      title: "Portfolio",
+      description: "Site vitrine personnel, présentant mes compétences, mes projets et mes réalisations.",
+      imageSrc: "/images/odd.png",
+      tags: [
+        { id: 16, name: "React" },
+        { id: 17, name: "Next.js" },
+        { id: 18, name: "Tailwind" },
+        { id: 19, name: "Typescript" },
+      ],
+      links: [
+        { id: 6, name: "Website", url: "https://sauretmathieu.github.io/portfolio/", icon: "Website" },
+        { id: 7, name: "Front/Back", url:"https://github.com/SAURETMathieu/portfolio", icon:"Github" },
+      ],
+    },
+  ]
+
+  return (
+    <section className="w-full max-w-screen-xl text-center mt-8 mb-4 mx-auto">
+      <h3 className="text-3xl font-bold mb-6">PROJETS</h3>
+      <div className="grid place-items-center gap-3 sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-4">
+      {projectsCards.map((project) => (
+          <ProjectCard
+            key={project.title}
+            title={project.title}
+            description={project.description}
+            imageSrc={project.imageSrc}
+            tags={project.tags}
+            links={project.links}
+          />
+        ))}
+      </div>
+    </section>
+  );
+}
+
+export default ProjectSection;
