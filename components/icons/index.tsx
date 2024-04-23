8ee94ea77980371a17b4b5c8466ab1665c9e1272
SAURETMathieu <mathieu.sauret@oclock.school>
2024-04-23 13:22:29 +0200

    feat(icons): add icons for skills

--- components/icons/index.tsx
+++ components/icons/index.tsx
@@ -11,8 +11,29 @@ import { DocusaurusIcon } from "./DocusaurusIcon";
 import { NextjsIcon } from "./NextjsIcon";
 import { GithubIcon } from "./GithubIcon";
 import { WebsiteIcon } from "./WebsiteIcon";
-// import { TypescriptIcon } from "./TypescriptIcon";
+import { TypescriptIcon } from "./TypescriptIcon";
 import { MarkdownIcon } from "./MarkdownIcon";
+import { TailwindIcon } from "./TailwindIcon";
+import { ExpressIcon } from "./ExpressIcon";
+import { SequelizeIcon } from "./SequelizeIcon";
+import { ApiIcon } from "./ApiIcon";
+import { PooIcon } from "./PooIcon";
+import { PhpIcon } from "./PhpIcon";
+import { VscIcon } from "./VscIcon";
+import { GitIcon } from "./GitIcon";
+import { ScrumIcon } from "./ScrumIcon";
+import { CopilotIcon } from "./CopilotIcon";
+import { CodaIcon } from "./CodaIcon";
+import { WhimsicalIcon } from "./WhimsicalIcon";
+import { TrelloIcon } from "./TrelloIcon";
+import { CuriousIcon } from "./CuriousIcon";
+import { AutodidactIcon } from "./AutodidactIcon";
+import { LogicIcon } from "./LogicIcon";
+import { TeamIcon } from "./TeamIcon";
+import { VersatileIcon } from "./VersatileIcon";
+import { GeniousIcon } from "./GeniousIcon";
+import { PerfectIcon } from "./PerfectIcon";
+import { SeriousIcon } from "./SeriousIcon";
 
 export const iconComponents = {
   React: ReactIcon,
@@ -29,5 +50,26 @@ export const iconComponents = {
   Github: GithubIcon,
   Website: WebsiteIcon,
   Markdown: MarkdownIcon,
-  // "Typescript": TypescriptIcon,
+  "Typescript": TypescriptIcon,
+  "Tailwind": TailwindIcon,
+  "Express": ExpressIcon,
+  "Sequelize": SequelizeIcon,
+  "API Rest": ApiIcon,
+  "POO": PooIcon,
+  "PHP": PhpIcon,
+  "VSC": VscIcon,
+  "Git": GitIcon,
+  "Scrum": ScrumIcon,
+  "IA": CopilotIcon,
+  "Coda": CodaIcon,
+  "Whimsical": WhimsicalIcon,
+  "Trello": TrelloIcon,
+  "Curieux": CuriousIcon,
+  "Autodidacte": AutodidactIcon,
+  "Logique": LogicIcon,
+  "Collaboration": TeamIcon,
+  "Polyvalent": VersatileIcon,
+  "Ingénieux": GeniousIcon,
+  "Perfectionniste":PerfectIcon,
+  "Sérieux": SeriousIcon,
 };
