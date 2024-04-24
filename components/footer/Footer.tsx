fcb5578e7e02d76404178d7102a1d3da5222d38d
SAURETMathieu <mathieu.sauret@oclock.school>
2024-04-24 19:53:58 +0200

    feat(footer): create and adding footer to the page

---
+++ components/footer/Footer.tsx
@@ -0,0 +1,31 @@
+import { NextjsIcon } from "../icons/NextjsIcon";
+import { ReactIcon } from "../icons/ReactIcon";
+import { TailwindIcon } from "../icons/TailwindIcon";
+import LinkCard from "../main/LinkCard";
+
+export function Footer() {
+  return (
+    <footer className="w-full flex flex-col justify-center items-center gap-2 p-10 bg-accent text-current">
+      <LinkCard/>
+      <p>© 2024 Mathieu SAURET </p>
+      <p className="mb-4">Tous droits réservés.</p>
+      <p>Réalisé avec</p>
+      <div className="flex justify-center">
+        <span className="font-bold text-xs bg-primary/20 border border-accent p-1 rounded inline-flex items-center gap-2 m-1 mt-2">
+          React
+          <ReactIcon size={16} className="inline" />
+        </span>
+        <span className="font-bold text-xs bg-primary/20 border border-accent p-1 rounded inline-flex items-center gap-2 m-1 mt-2">
+          Tailwind
+          <TailwindIcon size={16} className="inline" />
+        </span>
+        <span className="font-bold text-xs bg-primary/20 border border-accent p-1 rounded inline-flex items-center gap-2 m-1 mt-2">
+          Next.js
+          <NextjsIcon size={16} className="inline" />
+        </span>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
