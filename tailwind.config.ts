fa77d2769b81c55473bce23351ff64c3be763d99
SAURETMathieu <mathieu.sauret@oclock.school>
2024-04-24 19:52:12 +0200

    style(global): add animation to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -74,6 +74,7 @@ const config = {
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "shake": "shake 0.2s linear",
       },
     },
   },
